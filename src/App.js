047b08b4b73c5a3651e524002906e65e9000f1e3
Vitor Wagner <vitormwagner@gmail.com>
2022-10-26 15:08:52 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Form from './components/Form';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Tryunfo</h1>
-      </div>
+      <>
+        <div>
+          <h1>Tryunfo</h1>
+        </div>
+        <Form />
+      </>
     );
   }
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 class Form extends Component {
-  state = {  } 
-  render() { 
-    return ();
+  render() {
+    return (
+      <section>
+        <input type="text" data-testid="name-input" name="" id="" />
+        <input type="textarea" data-testid="description-input" />
+        <input type="number" data-testid="attr1-input" name="" id="" />
+        <input type="number" data-testid="attr2-input" name="" id="" />
+        <input type="number" data-testid="attr3-input" name="" id="" />
+        <input type="text" data-testid="image-input" name="" id="" />
+        <label htmlFor="Rarity">
+          Raridade:
+          <select data-testid="rare-input" id="Rarity">
+            <option value="normal">Normal</option>
+            <option value="raro">Raro</option>
+            <option value="muito raro">Muito raro</option>
+          </select>
+        </label>
+        <input type="checkbox" data-testid="trunfo-input" name="" id="" />
+        <button type="submit" data-testid="save-button">Salvar</button>
+      </section>
+    );
   }
 }
- 
+
 export default Form;
