15b29dc42348cab5e12c753ee12812e9a65d0d69
Vitor Wagner <vitormwagner@gmail.com>
2023-02-19 17:15:08 -0300

    feat: :sparkles: added name filter for deck cards

--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -3,35 +3,56 @@ import PropTypes from 'prop-types';
 import Card from './Card';
 
 class Deck extends Component {
+  state = {
+    nameQuery: '',
+  };
+
+  handleNameFilter = (event) => {
+    console.log(event.target.value);
+    this.setState({ nameQuery: event.target.value });
+  };
+
   render() {
     const { deck, handleDelete } = this.props;
 
+    const { nameQuery } = this.state;
+
+    const deckFiltered = deck.filter((card) => card.name.includes(nameQuery));
+
     return (
       <>
-        {deck.map((card) => (
-          <div key={ card.name }>
-            <div>
-              <Card
-                cardName={ card.name }
-                cardDescription={ card.description }
-                cardAttr1={ card.attr1 }
-                cardAttr2={ card.attr2 }
-                cardAttr3={ card.attr3 }
-                cardImage={ card.imgUrl }
-                cardRare={ card.rare }
-                cardTrunfo={ card.trunfo }
-              />
+        <input
+          value={ nameQuery }
+          name="nameQuery"
+          type="text"
+          onChange={ this.handleNameFilter }
+          data-testid="name-filter"
+        />
+        <div>
+          {deckFiltered.map((card) => (
+            <div key={ card.name }>
+              <div>
+                <Card
+                  cardName={ card.name }
+                  cardDescription={ card.description }
+                  cardAttr1={ card.attr1 }
+                  cardAttr2={ card.attr2 }
+                  cardAttr3={ card.attr3 }
+                  cardImage={ card.imgUrl }
+                  cardRare={ card.rare }
+                  cardTrunfo={ card.trunfo }
+                />
+              </div>
+              <button
+                type="button"
+                data-testid="delete-button"
+                onClick={ () => handleDelete(card.name, card.trunfo) }
+              >
+                Excluir
+              </button>
             </div>
-            <button
-              type="button"
-              data-testid="delete-button"
-              onClick={ () => handleDelete(card.name, card.trunfo) }
-            >
-              Excluir
-
-            </button>
-          </div>
-        ))}
+          ))}
+        </div>
       </>
     );
   }
